4e15553f669be1337122e6bd54eea0dcf04333f3
oliver.mrazik <olivermrazik@gmail.com>
2017-07-06 11:01:11 +0200

    S01-L14: Export Statements
    - added search bar input field and render it on a page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAUGZujqvhPjk_RQzDXUC9tyur2TwdS514';
 
 // Create a new Component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and put it on a the page (in the DOM)
